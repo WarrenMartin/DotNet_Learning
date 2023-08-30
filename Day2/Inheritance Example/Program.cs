589789528016ff0bda2b7f35cdfcaf467708aac5
WarrenMartin <87016961+WarrenMartin@users.noreply.github.com>
2023-08-31 01:39:16 +0530

    Update Program.cs

--- Day2/Inheritance Example/Program.cs
+++ Day2/Inheritance Example/Program.cs
@@ -1,21 +1,22 @@
-﻿namespace Inheritance_Example
+namespace Inheritance_Example
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            BaseClass o=new BaseClass();
+            o.PUBLIC=6;
+            o.PROTECTED_INTERNAL=9;
+            o.INTERNAL=10;
+
+            // the not allowed once are
+           // o.PRIVATE; Same class accessible 
+            // o.PROTECTED; accessible in derived class
+           // o.PRIVATE_PROTECTED; accessible in same class and derived class 
+
+
+            DerivedCLass o2=new DerivedCLass();
             
         }
     }
-}
-
-
-public class BaseClass
-{
-    public int i;
-}
-
-public class DerivedCLass : BaseClass
-{ 
-    
 }
